9fc1b6cfe79f9d84735e547aff3c16c96bd8c0ee
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2023-09-28 21:30:25 +0300

    added fetchUsers thunkCreator

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,12 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../thunks/fetchUsers";
 
 const usersSlice = createSlice({
     name: 'users',
     initialState: {
-        data: []
+        data: [
+            {"id": 1, "name": "Andrew"}
+        ],
+        isLoading: false,
+        error: null
     },
-    reducers: {
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.pending, (state, action) => {
+            //action.payload === response.data
+            state.isLoading = true
 
+        })
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            state.isLoading = false
+        })
+        builder.addCase(fetchUsers.rejected, (state, action) => {
+            state.isLoading = false
+            state.error = action.error
+        })
     }
 })
 
