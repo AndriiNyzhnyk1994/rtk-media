b922afee53349c5b77ee6e956d988e6c634b615a
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2023-10-04 02:53:31 +0300

    added the expandable panel

--- src/components/ExpandeblePanel.js
+++ src/components/ExpandeblePanel.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-function ExpandeblePanel() {
-  return (
-    <div>ExpandeblePanel</div>
-  )
+function ExpandeblePanel({ header, children }) {
+    return (
+        <div className="mb-2 border rounded">
+            <div className="flex p-2 justify-between items-center cursor-pointer">
+                <div className='flex flex-row items-center justify-between '>
+                {header}
+                </div>
+            </div>
+            <div className='p-2 border-t'>
+                {children}
+            </div>
+        </div>
+    )
 }
 
 export default ExpandeblePanel
--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -2,6 +2,7 @@ import { GoTrash } from 'react-icons/go'
 import Button from './Button'
 import { removeUser } from '../store'
 import { useThunk } from '../hooks/useThunk'
+import ExpandeblePanel from './ExpandeblePanel'
 
 function UsersListItem({ user }) {
 
@@ -11,17 +12,19 @@ function UsersListItem({ user }) {
         doRemoveUser(user)
     }
 
-    return <div className="mb-2 border rounded">
-        <div className="flex p-2 justify-between items-center cursor-pointer">
-            <div className='flex flex-row items-center justify-between '>
-                <Button className='mr-3' loading={isLoading} onClick={handleClick}>
-                    <GoTrash />
-                </Button>
-                {error && <div>Error deleting user</div>}
-                {user.name}
-            </div>
-        </div>
-    </div>
+    const header = <>
+        <Button className='mr-3' loading={isLoading} onClick={handleClick}>
+            <GoTrash />
+        </Button>
+        {error && <div>Error deleting user</div>}
+        {user.name}
+    </>
+
+    return (
+        <ExpandeblePanel header={header}>
+            CONTENT
+        </ExpandeblePanel>
+    )
 }
 
 export default UsersListItem
