ac71c60e183ad542b18b819ac589b562214699d9
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2023-10-04 21:40:38 +0300

    added chevrons (down&left)

--- src/components/ExpandeblePanel.js
+++ src/components/ExpandeblePanel.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useStore } from 'react-redux'
+import { GoChevronLeft } from 'react-icons/go'
+import { GoChevronDown } from 'react-icons/go'
+
 
 function ExpandeblePanel({ header, children }) {
+
+    const [expanded, setExpanded] = useState(false)
+
+    const handleClick = () => {
+        setExpanded(!expanded)
+    }
+
     return (
         <div className="mb-2 border rounded">
-            <div className="flex p-2 justify-between items-center cursor-pointer">
+            <div className="flex p-2 justify-between items-center">
                 <div className='flex flex-row items-center justify-between '>
-                {header}
+                    {header}
+                </div>
+                <div onClick={handleClick} className='cursor-pointer'>
+                    {expanded ? <GoChevronDown /> : <GoChevronLeft />}
                 </div>
             </div>
-            <div className='p-2 border-t'>
+            {expanded && <div className='p-2 border-t'>
                 {children}
-            </div>
+            </div>}
+            
         </div>
     )
 }
