04e90a4c115b290a5c4e76c5b1fafe6a1fb93382
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2023-10-05 00:43:27 +0300

    added 'albumsApi.js' file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {
-        keyframes: {
-          shimmer: {
-            '100%': {transform: 'translateX(100%)'}
-          }
-        },
-        animation: {
-          shimmer: 'shimmer 2s infinite'
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      keyframes: {
+        shimmer: {
+          '100%': { transform: 'translateX(100%)' },
         },
       },
+      animation: {
+        shimmer: 'shimmer 1.5s infinite',
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
