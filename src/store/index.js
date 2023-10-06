daa8f4f3b3924dc2b94b2072ad73d7e02fb8f549
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2023-10-06 23:58:33 +0300

    added some comments

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
-
+import {setupListeners} from '@reduxjs/toolkit/query'
+import { albumsApi } from "./apis/albumsApi";
 
 export const store = configureStore({
     reducer: {
-        users: usersReducer
+        users: usersReducer,
+        albums: albumsApi.reducer
     }
 })
-
+// key 'albums' must be equal to reducerPath of albumsApi 
+// otherwise (в противном случае) our cod will not work
+// so we needn't { 'albums': albumsApi.reducer }
+// we need { [albumsApi.reducerPath]: albumsApi.reducer } 
  
+
 export * from './thunks/fetchUsers'
 export * from './thunks/addUser'
 export * from './thunks/removeUser'
